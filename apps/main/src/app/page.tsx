6890aec52a4053026685b265e13f1558c68bc5d1
maxwiseman <max@maxwiseman.io>
2025-05-31 13:48:42 -0400

    chore: update all dependencies to latest

--- apps/main/src/app/page.tsx
+++ apps/main/src/app/page.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@acme/ui/button";
+import { Card, CardHeader, CardTitle } from "@acme/ui/card";
+
 export default function Page() {
   return (
-    <div>Hello, world!</div>
-  )
+    <div>
+      <Button>Hello</Button>
+      Hello, world!
+      <Card>
+        <CardHeader>
+          <CardTitle>Hello</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
--- packages/ui/src/select.tsx
+++ packages/ui/src/select.tsx
@@ -7,7 +7,8 @@ import {
   ChevronUpIcon,
 } from "@radix-ui/react-icons";
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { cn } from "src/lib/utils";
+
+import { cn } from ".";
 
 const Select = SelectPrimitive.Root;
 
