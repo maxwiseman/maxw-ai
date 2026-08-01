9f2147314bd0e03083328f9b2d812036ccceb053
maxwiseman <max@maxwiseman.io>
2026-08-01 16:35:26 -0400

    Remove hardcoded browser user agent

--- apps/autopilot-backend/src/index.ts
+++ apps/autopilot-backend/src/index.ts
@@ -29,7 +29,6 @@ const args = [
   // "--incognito",
   "--ignore-certifcate-errors",
   "--ignore-certifcate-errors-spki-list",
-  '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
 ];
 
 const options: LaunchOptions = {
